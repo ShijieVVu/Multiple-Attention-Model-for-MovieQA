2093364750e66cbc794aa7cef9789f497a796177
Shijie <wushijiedu@gmail.com>
2018-04-11 23:07:34 -0400

    Complete data generator and baseline model for MovieQA.

--- baseline.py
+++ baseline.py
@@ -1,5 +1,5 @@
 from keras.layers import Conv2D, MaxPooling2D, Flatten
-from keras.layers import Input, LSTM, Embedding, Dense, concatenate, Lambda
+from keras.layers import Input, LSTM, Embedding, Dense, concatenate, Lambda, GRU
 from keras.models import Model, Sequential
 from keras.layers import TimeDistributed
 from keras.optimizers import Adam
@@ -9,6 +9,7 @@ import numpy as np
 from os import path
 
 from pickle import load, dump
+from my_classes import DataGenerator
 
 vocab_size = 50000
 video_len = 10000
@@ -18,8 +19,10 @@ subtitle_len = 500
 
 def baseModel():
     encoder = Embedding(input_dim=vocab_size, output_dim=256, input_length=qa_len)
-    video_lstm = LSTM(256)
-    text_lstm = LSTM(256)
+    # video_lstm = LSTM(256)
+    # text_lstm = LSTM(256)
+    video_lstm = GRU(256)
+    text_lstm = GRU(256)
 
     vi = Input(shape=(video_len, 512))
     vp = video_lstm(vi)  # the output will be a vector
@@ -48,74 +51,17 @@ def baseModel():
     prediction = Dense(5, activation='softmax')(concatenate(scores))
     return Model([vi, qai, subt], outputs=prediction)
 
-
-def load_data(vocab_size=50000, video_len=10000, qa_len=40, subtitle_len=200):
-    """
-    Return the training video frames, questions and answers based on qa index.
-    :return:
-    """
-    if path.isfile(r"D:\train_data.p"):
-        return load(open(r'D:\train_data.p', 'rb'))
-    video_base = r'D:\data_processed'
-    subtt_base = r'D:\subtt'
-    import sys
-    sys.path.insert(0, r'C:\Users\WUSHI\github\MovieQA_benchmark')
-    import data_loader
-    mqa = data_loader.DataLoader()
-    vl_qa, qas = mqa.get_video_list('train', 'qa_clips')
-
-    qas = qas[:3]
-
-    videos = []
-    subtitles = []
-    for i, qa in enumerate(qas, 0):
-        if i % 100 == 0:
-            print(i)
-        video = []
-        subtt = []
-        for clip in qa.video_clips:
-            abs_path = r'{}\{}features.p'.format(video_base, clip)
-            frame_feature = load(open(abs_path, 'rb'))
-            video.extend(frame_feature.reshape(-1, 512))
-            abs_path = r'{}\{}.p'.format(subtt_base, clip)
-            lines = load(open(abs_path, 'rb'))
-            lines = [item for sublist in [one_hot(line, vocab_size) for line in lines] for item in sublist]
-            subtt.extend(lines)
-        video = np.expand_dims(np.array(video), axis=0)
-        video = pad_sequences(video, maxlen=video_len)
-        videos.append(video.squeeze(axis=0))
-        tmp = np.zeros((1, subtitle_len))
-        subtt = np.array(subtt).reshape(1, -1)
-        tmp[:, -subtt.shape[1]:] = subtt[:, :subtitle_len]
-        subtitles.append(tmp)
-    videos = np.array(videos)
-    subtitles = np.squeeze(np.array(subtitles), axis=1)
-
-    labels = []
-    question_answers = []
-    for qa in qas:
-        tmp = np.zeros(5)
-        tmp[qa.correct_index] = 1
-        labels.append(tmp)
-        question = qa.question
-        qas2 = []
-        for answer in qa.answers:
-            tmp = np.zeros((1, qa_len))
-            c = np.array(one_hot(question + answer, vocab_size)).reshape(1, -1)
-            tmp[:, -c.shape[1]:] = c
-            qas2.append(tmp)
-        np.array(qas2)
-        question_answers.append(qas2)
-    question_answers = np.array(question_answers).squeeze(2)
-    labels = np.array(labels).reshape(-1, 5)
-
-    dump((videos, subtitles, question_answers, labels), open(r"D:\train_data.p", 'wb'))
-    return videos, subtitles, question_answers, labels
-
-
-videos, subtitles, question_answers, labels = load_data(vocab_size=vocab_size, video_len=video_len, qa_len=qa_len,
-                                                        subtitle_len=subtitle_len)
 model = baseModel()
 
 model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
-model.fit([videos, question_answers, subtitles], [labels], epochs=1, batch_size=16)
+# model.fit([videos, question_answers, subtitles], [labels], epochs=1, batch_size=10)
+import sys
+sys.path.insert(0, r'C:\Users\WUSHI\github\MovieQA_benchmark')
+import data_loader
+mqa = data_loader.DataLoader()
+vl_qa, training_qas = mqa.get_video_list('train', 'qa_clips')
+training_qa = training_qas[:2000]
+training_generator = DataGenerator(training_qa, batch_size=8)
+validation_qa = training_qas[2000:2032]
+validation_generator = DataGenerator(validation_qa, batch_size=8)
+model.fit_generator(generator=training_generator, validation_data=validation_generator, use_multiprocessing=False, workers=1)
